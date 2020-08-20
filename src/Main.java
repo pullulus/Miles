0dd2f7acf4762adbfca3d88ac5e767097cfc74a4
Elena Tsypileva <etsypileva@gmail.com>
2020-08-20 18:28:55 +0300

    Added src

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticketPriceInKopecks = 945067;
+        long miles = ticketPriceInKopecks /100 /20;
+
+        System.out.println(miles);
+    }
+}
