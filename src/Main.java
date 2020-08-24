03ca8601f46a8e7f4e9065dcb753b5faf65495d9
Elena Tsypileva <etsypileva@gmail.com>
2020-08-24 10:44:38 +0300

    Improved Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        long ticketPriceInKopecks = 945067;
-        long miles = ticketPriceInKopecks /100 /20;
+        int ticketPriceInKopecks = 945067;
+        int miles = ticketPriceInKopecks / 100 / 20;
 
         System.out.println(miles);
     }
